ecb2c5bb7b3b45b103ba0dfb697abfbc2d1185c6
Brian Clymer <crdnilfan@gmail.com>
2014-01-03 23:37:17 -0600

    Add some more info.
    This isn't how I actually write JS, this is insanely hacky.

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,35 @@
 (function() {
 	var stats = [];
 	var enabled = [
+		"status",
 		"temperature",
 		"fanSpeed",
 		"fanPercent",
 		"mhsLastFiveSeconds",
 		"accepted",
+		"difficultyAccepted",
 		"rejected",
+		"difficultyRejected",
 		"utility",
-		"lastValidWork"
+		"lastValidWork",
+		"hardwareErrors"
 	]
 	$.get('/stats', function(data) {
 		machines = data;
-
 		var devicesContainer = $('#devices-container');
 		_.each(machines, function(machine, machineName) {
 			_.each(machine, function(device, deviceId) {
+				var currentStats = [];
 				devicesContainer.append('<div class="page-header"><h1>' + machineName + ' - ' + deviceId + '</h1></div>');
 				var containerId = machineName + '-' + deviceId;
 				devicesContainer.append('<div class="bs-example" id="' + containerId + '">');
 				var chartHolder = $('#' + containerId);
 				_.each(device, function(stats, key) {
 					if (enabled.indexOf(key) == -1) return true;
+					currentStats.push({
+						key: key,
+						value: stats[stats.length - 1]
+					});
 					var id = machineName+'.'+deviceId+'.'+key;
 					var canvas = $('<div/>')
 						.width(800).height(400)
@@ -36,13 +44,26 @@
 						title: {
 							text: key.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })
 						},
+						axisY:{
+							includeZero: false,
+						},
 						data: [{
 							type: "line",
 							dataPoints: map
 						}]
 					});
 					chart.render();
-				})
+				});
+				var table = '<h2>Current Stats</h2><table border="1" cellpadding="5"><tr>';
+				_.each(currentStats, function(stat) {
+					table += '<th>' + stat.key.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); }) + '</th>';
+				});
+				table += '</tr><tr>';
+				_.each(currentStats, function(stat) {
+					table += '<td>' + stat.value + '</td>';
+				});
+				table += '</tr></table>';
+				chartHolder.prepend(table);
 			});
 		});
 	});
